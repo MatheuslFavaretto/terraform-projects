d15211e5314308afa76ca955350ffbc9dabc42b7
MatheuslFavaretto <matheusfavaretto.ef@gmail.com>
2023-04-16 18:15:47 -0300

    modificado .gitignore e README.MD.Criado diretorios azure-vm-com-vnet-remote-state e aws-vm-vpc-remote-state.

---
+++ azure-vm-com-vnet-remote-state/main.tf
@@ -0,0 +1,33 @@
+terraform {
+
+  required_version = ">= 1.0.0"
+
+  required_providers {
+
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.52.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "remote-state"
+    storage_account_name = "matheusremotestate"
+    container_name       = "remotestate"
+    key                  = "azure-vm/terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "terraform_remote_state" "vnet" {
+  backend = "azurerm"
+  config = {
+    resource_group_name  = "remote-state"
+    storage_account_name = "matheusremotestate"
+    container_name       = "remotestate"
+    key                  = "azure-vnet/terraform.tfstate"
+  }
+
+}
