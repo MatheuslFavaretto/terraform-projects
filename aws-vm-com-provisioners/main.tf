036ab9e4e26a1917667194b2f8b30ee70b816fdc
MatheuslFavaretto <matheusfavaretto.ef@gmail.com>
2023-04-17 19:27:32 -0300

    adicionado aws-vm-com-provioners #aprendendo a usar provisioners#.

---
+++ aws-vm-com-provisioners/main.tf
@@ -0,0 +1,38 @@
+terraform {
+
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.73.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "matheusf-remote-state"
+    key    = "aws-vm/terrform.tfstate"
+    region = "sa-east-1"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "sa-east-1"
+
+  default_tags {
+    tags = {
+      owner      = "MatheuslFavaretto"
+      managed-by = "terraform"
+    }
+  }
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "matheusf-remote-state"
+    key    = "aws-vpc/terrform.tfstate"
+    region = "sa-east-1"
+  }
+}
