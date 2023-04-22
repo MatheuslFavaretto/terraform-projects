282ed6c335c3ab3a15eef6c7d3747e3dd09a825a
MatheuslFavaretto <matheusfavaretto.ef@gmail.com>
2023-04-22 20:08:54 -0300

    adicioando diretorio pipeline-gihub-actions.

---
+++ pipeline-github-actions/main.tf
@@ -0,0 +1,57 @@
+terraform {
+
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.94.0"
+    }
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.73.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "remote-state"
+    storage_account_name = "matheusremotestate"
+    container_name       = "remote-state"
+    key                  = "pipeline-github-actions/terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "terraform_remote_state" "vnet" {
+  backend = "azurerm"
+  config = {
+    resource_group_name  = "remote-state"
+    storage_account_name = "matheusremotestate"
+    container_name       = "remote-state"
+    key                  = "azure-vnet/terraform.tfstate"
+  }
+}
+
+provider "aws" {
+  region = "sa-east-1"
+
+  default_tags {
+    tags = {
+      owner      = "matheusf"
+      managed-by = "terraform"
+    }
+  }
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "matehus-remote-state"
+    key    = "aws-vpc/terraform.tfstate"
+    region = "sa-east-1"
+  }
+}
