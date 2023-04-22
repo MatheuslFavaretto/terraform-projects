3b609825997ea87bad9b0770ee896ecb79241f21
MatheuslFavaretto <matheusfavaretto.ef@gmail.com>
2023-04-22 15:17:36 -0300

    adicionado diretorio aws-provider-vpc

---
+++ aws-provider-vpc/main.tf
@@ -0,0 +1,43 @@
+terraform {
+
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.73.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "matheusf-remote-state"
+    key    = "aws-vpc-provider/terrform.tfstate"
+    region = "sa-east-1"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "sa-east-1"
+
+  default_tags {
+    tags = {
+      owner      = "MatheuslFavaretto"
+      managed-by = "terraform"
+    }
+  }
+}
+
+provider "aws" {
+  alias  = "eua"
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      owner      = "MatheuslFavaretto"
+      managed-by = "terraform"
+    }
+  }
+}
+
+
